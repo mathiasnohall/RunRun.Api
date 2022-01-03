a97a8f5f20bba122e168229ab96796a54db93c0d
mathiasnohall <mathias.nohall@gmail.com>
2022-01-03 18:36:46 +0100

    cors

--- src/RunRun.Api/Program.cs
+++ src/RunRun.Api/Program.cs
@@ -1,4 +1,3 @@
-using Microsoft.AspNetCore.Cors.Infrastructure;
 using RunRun.Api.Repositories.v1;
 using RunRun.Api.Services.v1;
 
@@ -12,7 +11,10 @@ builder.WebHost.UseUrls("http://localhost:5443");
 builder.Services.AddControllers();
 builder.Services.AddMvc();
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddCors(c => c.AddPolicy("apiOrigin", b => b.WithOrigins("https://mqpgzmdssy.eu-west-1.awsapprunner.com", "http://localhost:3000")));
+builder.Services.AddCors(c => c.AddPolicy("apiOrigin", 
+    b => b.WithOrigins("https://mqpgzmdssy.eu-west-1.awsapprunner.com", "http://localhost:3000")
+        .AllowAnyMethod()
+        .AllowAnyMethod()));
 builder.Services.AddSwaggerGen();
 builder.Services.AddTransient<IOrderService, OrderService>();
 builder.Services.AddTransient<IOrderRepository, OrderRepository>();
